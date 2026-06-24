95c4e18e3261d5e91df284e8bfb5729125b25080
mmathur16 <mmathur_be24@thapar.edu>
2026-06-24 23:54:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
 import streamlit as st
-import pandas as pd
-from pathlib import Path
+import os
 
-csv_path = Path(__file__).parent / "All Data Main Trans (1).csv"
-
-st.write("Looking for:", csv_path)
-st.write("Exists:", csv_path.exists())
-
-df = pd.read_csv(csv_path, low_memory=False)
-
-st.dataframe(df)
+st.write("Current folder:", os.getcwd())
+st.write("Files here:", os.listdir())
